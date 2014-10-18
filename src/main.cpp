a2f60d74bd4bceec637d7842815d0c6f867922c9
Paul Starkey <pstar001@ucr.edu>
2014-10-17 20:53:33 -0700

    Attempting to parse... The double parse is limiting parsing to only a single token

--- src/main.cpp
+++ src/main.cpp
@@ -26,39 +26,62 @@ int main ()
     }
     else if (pid == 0)
     {
-        cout << "hey" << endl;
+        //testing of mine for execvp... DELETE in final build
+        /*    
         char *argv[3];
         argv[0] = new char[3];
         strcpy(argv[0], "ls");
         //rgv[1] = new char[3];
         argv[1] = "-l";
-        //argv[2] = new char[3];
-        argv[2] = "-a";
         execvp("ls",argv);
+        */
         cout << "Child process" << endl;
         while (1)
         {
             //infinite loop to run until exit command
+
             string parse="";
             cout << "$ ";
             getline(cin, parse);
-            tokenizer<> tok(parse);
-            for(tokenizer<>::iterator beg=tok.begin(); beg != tok.end(); ++beg)
+            char del[2] = ";"; //delimiter to signal diff cmd
+            char* token=0; //point to null initially
+                           //token will be parser
+            char* replace=0; //point to null
+            replace = new char[parse.size()+1];
+            memcpy(replace, parse.c_str(), parse.size()+1);
+            
+            token = strtok(replace, del);
+            while (token != NULL)
             {
-                //where eventual parsing will go down
-                if(*beg == "exit") //if usser wants to quit, enter `exit`
-                {
-                    exit(1);
-                }
-                else if(*beg == "#")
-                {
-                    cout << "hey" << endl;
-                    break;
-                }
-            }
-        }
+                //printf( " %s\n", token);
+                cout << "token: " <<  token << endl;
+                
+                string fakeTok = token;
+                cout << "faketok: " << fakeTok << endl;
 
-    }
+                char* replace2=0;
+                replace2 = new char[fakeTok.size()+1];
+
+                char delim2[2] = " "; //to parse further.. take out spaces
+                memcpy(replace2, fakeTok.c_str(), fakeTok.size()+1);
+               
+                char* tok2=0; 
+                tok2 = strtok(replace2, delim2);
+                cout << token << endl;
+                while(tok2 != NULL)
+                {
+                    cout << tok2 << endl;
+                    tok2 = strtok(NULL, delim2);
+                }
+                token = strtok(NULL, del);//inc token to next grouping
+                //execvp(token[0], token);
+            }
+
+                //I need these two conditions. REMINDER
+                //if(*beg == "exit") //if usser wants to quit, enter `exit`
+                //else if(*beg == "#")
+         }
+     }    
     else if (pid > 0)
     {   
         if( -1 == wait(0))
