8617d025dbd117f6115b34beead532bd40449528
Paul Starkey <pstar001@ucr.edu>
2014-10-19 17:16:36 -0700

    Cleaned up the code. No new functionality

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@ using namespace std;
 int main ()
 {
     int pid = fork();
-    if(pid == -1) //throw an error if fork returns -1
+    if(pid == -1) 
     {
         perror("fork outside:");
         exit(1); 
@@ -24,8 +24,19 @@ int main ()
         while (1)
         {
             //infinite loop to run until exit command
+            char* login = getlogin();
+            if ( 0 == getlogin())
+            {
+                perror("getLogin()");
+            }
+
+            char user[256];
+            if (-1 == gethostname(user, sizeof(user)))
+            {
+                perror("gethostname");
+            }
             string parse="";
-            cout << "$ ";
+            cout << login << user <<   "$ ";
             getline(cin, parse);
             char del[] = ";"; //delimiter to signal diff cmd
             char *token, *token2;
@@ -36,15 +47,15 @@ int main ()
             memcpy(replace, parse.c_str(), parse.size()+1);
             
             token = strtok_r(replace, del, &savptr1);
+            if (token == "exit" ) 
+             {
+                cout << "die" << endl;
+             }
+                    
             while (token != NULL)
             {
-                cout << "token: " <<  token << endl;
+                //cout << "token: " <<  token << endl;
                 string fakeTok = token;
-                /*if (token == "exit")
-                {
-                    cout << "exit" << endl;
-                    exit(1);
-                }*/
                 char* replace2=0;
                 replace2 = new char[fakeTok.size()+1];
 
@@ -56,22 +67,18 @@ int main ()
                 while(token2 != NULL)
                 {
                     hold.push_back(token2);
-                    //check doesn't work yet. Trying to ignore after #
-                    if(token2 == "exit")
-                            {   
-                                cout << "fuck" << endl;
-                                exit(1);
-                            }
                     //cout << "loop: " << token2 << endl;
                     token2 = strtok_r(NULL, delim2, &savptr2);
                 }
-                //cout << "token2:" << token << endl;
                 //loop to get parsed data into array for execvp
                 char *argv[hold.size()+1];
                 
                 for(unsigned i=0; i<hold.size();i++)
                 {
-                   //cout << "hold: " << hold.at(i) << endl;
+                   if(hold.at(i) == "exit")
+                   {
+                        exit(1);
+                   }
                    argv[i] = new char[hold.at(i).size()+1];
                    strcpy(argv[i], hold.at(i).c_str());
                 }
@@ -84,11 +91,14 @@ int main ()
                 }
                 else if (pid2 == 0)
                 {
+                    cout << "HERE" << endl;
+                    cout << argv[0] << endl;
                     execvp(argv[0], argv);
                     perror("execvp");
+                    exit(1);
                 }
                 else 
-                {
+                {   
                     if(-1 == wait(0))
                     {
                         perror("wait()");
@@ -103,8 +113,10 @@ int main ()
      }    
     else if (pid > 0)
     {   
-        if( -1 == wait(0))
+        int status2 = 2;
+        if( -1 == wait(&status2))
         {
+            if(status2 > 2) { exit(1);}
             perror("wait");
         }
         cout << "Parent process" << endl;
