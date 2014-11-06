a4dca8bb03d4cf437f6bce86b35ae0f7ad4f8da0
Paul Starkey <pstar001@ucr.edu>
2014-11-06 15:31:54 -0800

    added multiple features. Can handle flags and paths

--- src/ls.cpp
+++ src/ls.cpp
@@ -34,7 +34,6 @@ bool compFunct(dirent* a, dirent* b)
         return toupper(a->d_name[i]) < toupper(b->d_name[j]); 
     }
 
-
 int main(int argc, char** argv)
 {
     string lsID = "ls";
@@ -47,21 +46,81 @@ int main(int argc, char** argv)
     }
     else
     {
+        //cerr << "made it " << endl;
         if(argc >= 2) //this means user entered ls and some args
         {
-            
-            const char *dirName = "."; //must be for curr dur name
-            DIR *dirp = opendir(dirName);
-            if(dirp == NULL)
+            const char* dirName=0;
+            const char* dot= ".";
+            int aFlag=0, lFlag=0, rFlag=0;
+            vector<const char*> pathHold;
+            vector<const char*> flagHold;
+            //These checks will determine if user passes in path name or not
+            if(argc == 2) //user only enters ls
             {
+                //dirName=dot;
+                pathHold.push_back(".");
+                cerr << "only ls provided" << endl;
+            }
+            /*else if(argv[2][0] == '-') //|| argv[2][0] != '/')
+            { //this implies that no path passed into ls. just "ls -flags"
+                dirName = dot;
+                cerr << "no path provided" << endl;
+            }*/
+            else
+            {
+                for(int i=2; i < argc; i++)
+                {
+                    if(argv[i][0] == '/')
+                    {
+                        pathHold.push_back(argv[i]);
+                    }
+                    if(argv[i][0] == '-')
+                    {
+                        flagHold.push_back(argv[i]);
+                        cerr << "Pushed flag " << endl;
+
+                        for(int j=0, k=1; j < flagHold.size(); j++,k++)
+                        {
+                            if(flagHold.at(j)[k] == 'a')
+                            {
+                                aFlag =1;
+                                cerr << "a flag set" << endl; 
+                            }
+                            if(flagHold.at(j)[k]=='R')
+                            {
+                                rFlag=1;
+                                cerr << "r flag set" << endl;
+
+                            }
+                            if(flagHold.at(j)[k]=='l')
+                            {
+                                lFlag=1;
+                                cerr << "l flag set" << endl;
+
+                            }
+                            //cerr << "Sup " << endl;
+                        }
+                        flagHold.clear();
+                    }
+                    
+                }
+                //dirName = argv[2]; //-- ./a.out ls path
+                                   //to assign path properly
+            }
+            
+            //THIS SHOULD BE IN LOOP... B/c IF USER ENTERS MULTIPLE PATHS
+            for(int i=0; i < pathHold.size(); i++)
+            {
+                DIR *dirp = opendir(pathHold.at(i));
+                if(dirp == NULL)
+                {
                     perror("opendir");
                     exit(1);
-            }
-            dirent *direntp;
-            vector<dirent*> dirHold;
-            vector<dirent> dirHold2;
-            while((direntp = readdir(dirp))) //put dir names into container for later sorting ease
-            {
+                }
+                dirent *direntp;
+                vector<dirent*> dirHold;
+                while((direntp = readdir(dirp))) //put dir names into container for later sorting ease
+                {
                     if(NULL == direntp)
                     {
                         perror("readdir");
@@ -69,21 +128,23 @@ int main(int argc, char** argv)
                     }
                     //cout << direntp->d_name << endl;
                     dirHold.push_back(direntp);
-            }
-            //cout << dirHold.at(1); 
-            
-            sort(dirHold.begin(), dirHold.end(), compFunct);
-            for(int i = 0; i < dirHold.size(); i++)
-            {
-                if(dirHold.at(i)->d_name[0] == '.')
-                {
-                   cout << dirHold.at(i)->d_name << '/' << endl; 
                 }
-                else
-                    cout << dirHold.at(i)->d_name << endl;
-            }
+                //cout << dirHold.at(1); 
+            
+                sort(dirHold.begin(), dirHold.end(), compFunct);
+                for(int i = 0; i < dirHold.size(); i++)
+                {
+                    if(dirHold.at(i)->d_name[0] == '.') //&& -a flag
+                    {
+                        cout << dirHold.at(i)->d_name << '/' << endl; 
+                    }
+                    else
+                        cout << dirHold.at(i)->d_name << endl;
+                }
 
-            closedir(dirp);
+                closedir(dirp);
+            }
+            
 
          }   
         
